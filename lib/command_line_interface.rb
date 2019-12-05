8cc2a730b8e31dd6c1e2bd3285111d652611c765
jmknopf1007 <jmknopf1007@gmail.com>
2019-12-05 12:47:32 -0800

    press any key functionality added to display_movies method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -54,7 +54,7 @@ class CommandLineInterface
         space_helper(2) 
         puts "(6) Rate a movie."
         space_helper(2) 
-        puts "(7) Find average rating for movie"
+        puts "(7) Find average rating for movie." 
         space_helper(2) 
         puts "(8) Exit this program." 
         space_helper(2) 
@@ -109,29 +109,41 @@ class CommandLineInterface
         movies = Movie.all.each do |movie|
             puts "#{movie.id}: #{movie.title}: #{movie.description}: #{movie.genre}"  
             space_helper(2)
-        end
-        #space_helper(2) 
-        puts "Press r to return to the homepage.".yellow
-        space_helper(2)
-        display_movie_input = gets.chomp
-
-        if display_movie_input != "r"
-        space_helper(2) 
-        puts "I'm sorry please press r to return to the homepage.".yellow 
-        display_movies
-        elsif display_movie_input == "r"
-        space_helper(2)
-        options 
         end 
+        puts "Press any key to return to the homepage.".yellow 
+        space_helper(2)
+        STDIN.getch 
+        options 
     end
+
+    # def display_movies
+    #     space_helper(2) 
+    #     movies = Movie.all.each do |movie|
+    #         puts "#{movie.id}: #{movie.title}: #{movie.description}: #{movie.genre}"  
+    #         space_helper(2)
+    #     end
+    #     #space_helper(2) 
+    #     puts "Press 'r' to return to the homepage.".yellow
+    #     space_helper(2)
+    #     display_movie_input = gets.chomp
+
+    #     if display_movie_input != "r"
+    #     space_helper(2) 
+    #     puts "I'm sorry please press r to return to the homepage.".yellow 
+    #     display_movies
+    #     elsif display_movie_input == "r"
+    #     space_helper(2)
+    #     options 
+    #     end 
+    # end
 
     def add_movie
         space_helper(2)
         puts "Please enter your username.".yellow 
         space_helper(2) 
-        username_info = gets.chomp
+        username_info = gets.chomp.downcase 
         space_helper(2) 
-        @find_user = User.find_by(username: username_info) 
+        @find_user = User.find_by(username: username_info.titleize)  
         if @find_user == nil 
         puts "I'm sorry that user doesn't exist. Please create a new account.".yellow
         space_helper(2) 
@@ -144,11 +156,11 @@ class CommandLineInterface
                 space_helper(2) 
                 puts "Please enter a description.".yellow
                 space_helper(2) 
-                movie_description = gets.chomp
+                movie_description = gets.chomp.downcase 
                 space_helper(2) 
                 puts "Please enter a genre.".yellow 
                 space_helper(2) 
-                movie_genre = gets.chomp
+                movie_genre = gets.chomp.downcase 
                 space_helper(2) 
                 puts "Movie added!".yellow 
                 Movie.create(title: movie_name.titleize, description: movie_description, genre: movie_genre) 
@@ -180,7 +192,7 @@ class CommandLineInterface
         space_helper(2) 
         puts "Please enter a username.".yellow
         space_helper(2) 
-        username = gets.chomp
+        username = gets.chomp.downcase
         space_helper(2) 
         puts "Please enter a age.".yellow  
         space_helper(2) 
@@ -188,7 +200,7 @@ class CommandLineInterface
         space_helper(2) 
         puts "User created!".yellow 
         space_helper(2) 
-        User.create(username: username, age: age_input)
+        User.create(username: username.titleize, age: age_input) 
         options   
     end
 
@@ -196,9 +208,9 @@ class CommandLineInterface
         space_helper(2)
         puts "Please enter your username.".yellow 
         space_helper(2) 
-        username_info = gets.chomp
+        username_info = gets.chomp.downcase
         space_helper(2) 
-        @find_user = User.find_by(username: username_info) 
+        @find_user = User.find_by(username: username_info.titleize)  
         #binding.pry 
         if @find_user == nil 
         puts "I'm sorry that user doesn't exist. Please create a new account.".yellow
@@ -239,7 +251,7 @@ class CommandLineInterface
         puts "(2) Delete user account"
         space_helper(2) 
 
-        u_info = gets.chomp
+        u_info = gets.chomp 
 
         if u_info == '1'
             space_helper(2) 
@@ -252,7 +264,7 @@ class CommandLineInterface
             delete_account
         else
             space_helper(2) 
-            puts "Command not found. Select 1 or 2.".yellow
+            puts "Command not found. Select '1' or '2' please.".yellow 
             edit_user_info
         end
     end
@@ -261,9 +273,9 @@ class CommandLineInterface
         space_helper(2) 
         puts "Please enter your username.".yellow 
         space_helper(2) 
-        username_info = gets.chomp
+        username_info = gets.chomp.downcase
         space_helper(2) 
-        @find_user = User.find_by(username: username_info) 
+        @find_user = User.find_by(username: username_info.titleize)  
             if @find_user == nil
                 puts "I'm sorry that user doesn't exist.".yellow
                 space_helper(2) 
@@ -274,13 +286,13 @@ class CommandLineInterface
                     space_helper(2) 
                     puts "Please enter a new username.".yellow 
                     space_helper(2) 
-                    new_name = gets.chomp 
+                    new_name = gets.chomp.downcase  
                     space_helper(2) 
                     puts "Please enter a new age.".yellow 
                     space_helper(2) 
                     new_age = gets.chomp 
                     space_helper(2) 
-                    new_profile = @find_user.update(username: new_name, age: new_age) 
+                    new_profile = @find_user.update(username: new_name.titleize, age: new_age) 
                     puts "Your new username is: #{@find_user.username}, and your new age is: #{@find_user.age}." 
                     space_helper(2)  
                     options 
@@ -293,9 +305,9 @@ class CommandLineInterface
         space_helper(2) 
         puts "Please enter your username.".yellow 
         space_helper(2) 
-        username_info = gets.chomp
+        username_info = gets.chomp.downcase
         space_helper(2) 
-        @find_user = User.find_by(username: username_info) 
+        @find_user = User.find_by(username: username_info.titleize)  
             if @find_user == nil
             puts "I'm sorry that user doesn't exist.".yellow
             space_helper(2) 
@@ -310,7 +322,7 @@ class CommandLineInterface
                     choice = gets.chomp
                     space_helper(2)
                     if choice != 'yes' && choice != 'no' 
-                        puts "Command not found, please type yes or no.".yellow 
+                        puts "Command not found, please type 'yes' or 'no' please.".yellow 
                         delete_account 
                     elsif choice == 'no'
                         puts "Sounds good! Thanks for staying with us we love you.".yellow 
